cd88ca191cd9bb80c3a76a51f6e3150dfd1c9bdc
cackharot <cackharot@gmail.com>
2016-12-31 16:52:16 -0500

    update health check script

--- health-check.sh
+++ health-check.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-curl -s lilly-dev-lilly-api-svc.lilly-dev.svc.walkure.net | grep Hello
+if [ $# != 3 ]; then
+    echo "Usage: $0 <stack> <appname> <env>"
+    echo "Examples:\n $0 lilly lilly-api ci\n"
+    exit 1
+fi
+
+curl -s $1-$2-svc.$1-$3.svc.walkure.net | grep Hello
